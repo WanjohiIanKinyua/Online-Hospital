5088c2ee6468dd0edce07c6d8b8bcd237ca2f9ca
Wanjohi Ian Kinyua <iankinyua322@gmail.com>
2026-02-20 17:00:33 +0300

    Add root API status route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,8 @@ app.use('/api/chat', chatRoutes);
 createMeetingSocket(io);
 
 // Health check
+app.get('/', (req, res) => res.send('API running'));
+
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'Server is running' });
 });
